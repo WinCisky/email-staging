4df0d63db81683eae4c6f79cae52018dfb991809
WinCisky <xsimone97@hotmail.it>
2024-12-08 22:04:09 +0100

    added username

--- src/routes/inbox/+page.svelte
+++ src/routes/inbox/+page.svelte
@@ -5,7 +5,8 @@
 
     // const endpoint = "http://localhost:3000";
     const endpoint = "https://test.opentrust.it";
-
+    
+    let username = $state("");
     let isMobileMenuOpen = $state(false);
     let isUserMenuOpen = $state(false);
     let isSelectedEmailMenuOpen = $state(false);
@@ -184,6 +185,7 @@
             window.location.href = `${base}/`;
         } else {
             const decodedUser = JSON.parse(user!);
+            username = decodedUser.username;
             const response = await fetch(
                 `${endpoint}/emails?username=${decodedUser.username}&password=${decodedUser.password}&page=${page}`,
             );
@@ -401,33 +403,6 @@
                     />
                 </form>
                 <div class="flex items-center gap-x-4 lg:gap-x-6">
-                    <button
-                        type="button"
-                        class="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
-                    >
-                        <span class="sr-only">View notifications</span>
-                        <svg
-                            class="h-6 w-6"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
-                            aria-hidden="true"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-                            />
-                        </svg>
-                    </button>
-
-                    <!-- Separator -->
-                    <div
-                        class="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10"
-                        aria-hidden="true"
-                    ></div>
-
                     <!-- Profile dropdown -->
                     <div class="relative">
                         <button
@@ -441,13 +416,13 @@
                             <span class="sr-only">Open user menu</span>
                             <img
                                 class="h-8 w-8 rounded-full bg-gray-50"
-                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                src="https://ui-avatars.com/api/?name={username}&background=random&rounded=true"
                                 alt=""
                             />
                             <span class="hidden lg:flex lg:items-center">
                                 <span
                                     class="ml-4 text-sm font-semibold leading-6 text-gray-900"
-                                    aria-hidden="true">Tom Cook</span
+                                    aria-hidden="true">{username}</span
                                 >
                                 <svg
                                     class="ml-2 h-5 w-5 text-gray-400"
